1bbc46adcdb8d5f8dbdbe4d185de62c6ad5104f1
Sagar Raut <ncs_Sagar.S@nfcguest.com>
2025-01-14 20:57:03 +0530

    Sign In/Sign Up form && Login Page/Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import "./App.css";
+import Body from "./components/Body";
 
 function App() {
   return (
     <div className="App">
-      <h1>Namaste Everyone</h1>
+      <Body />
     </div>
   );
 }
